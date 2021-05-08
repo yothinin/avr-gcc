163f4cc57bb3aa502e26df7f875deb5b85acc13e
yothinin <yothinin@hotmail.com>
2021-05-08 22:57:34 +0700

    Update: customize code to minimal

--- led.c
+++ led.c
@@ -5,32 +5,20 @@
 int main(void){
   DDRD  = 0x00;
   PORTD = 0x00;
+  
   DDRB  = 0xff;      //PB0-7 as output.  
   PORTB = 0xff;
 
   while (1){
     if((PIND&(1<<PIN7)) == (1<<PIN7)){
-	  if (PORTB == 0xff)
-        PORTB = 0xfe;
-        
+      PORTB = (PORTB == 0xff)?(PORTB <<1):(PORTB<<1)|1;          
       _delay_ms(50);        
-      PORTB = (PORTB << 1)|0x01;
-	  /*
-	  int i;
-	  for (i = 0; i<8; i++){
-        PORTB = ~(1<<i);
-		printf("%8x\n", PORTB);
-        fflush(stdout);
-        _delay_ms(100);
-	  }
-	  for (i = 7; i>=0; i--){
-		PORTB = ~(1<<i);
-		_delay_ms(100);
-	  }
-	  */
     }else{
 	  PORTB = 0xff;
 	}
   }
+  
   return 0;
 }
+
+
